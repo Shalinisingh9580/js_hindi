9b2d77c6cc1ed1616ed7dc3d4a1d04598cfc3112
Shalinisingh9580 <shalini071005@gmail.com>
2026-07-27 16:55:01 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Hello Shalini!");
+let name = "Shalini";
+let age = 21;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+let a = 10;
+let b = 20;
+
+console.log("Sum =", a + b);
