23b6370dac36763040244e0c042a99e72a158cd2
Shalinisingh9580 <shalini071005@gmail.com>
2026-07-27 17:34:46 +0000

    Added variables lecture

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "Shalini@gmail.com"
+var accountpassword = "12345"
+let accountcity = "Lucknow"
+
+accountEmail = "new@gmail.com"
+accountpassword = "99999"
+accountcity = "Delhi"
+console. table ([
+    accountId,
+    accountEmail,
+    accountpassword,
+    accountcity,
+])
